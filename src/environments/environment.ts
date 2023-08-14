305576cb270d5e48873b48cc9409ea6aa4174721
DiegoDeveloperES <diegodeveloperes@gmail.com>
2023-08-13 23:34:12 -0300

    att localhost1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  api:'http://localhost:3030'
-}
+  //api:'http://localhost:3030'
+  api: 'https://db7b-2804-d45-e03d-cf00-85a9-ba2a-f909-cb90.ngrok-free.app',
+};
