2b9db3fdeb66907915b7ad98b91da35c98a3eaf8
DiegoDeveloperES <diegodeveloperes@gmail.com>
2023-08-19 23:54:13 -0300

    ajuste logo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   //api:'/api/'
-  //api: 'http://localhost:3030', //Acessa os dados do backend
-  api: 'https://92f6-2804-d45-e063-a900-1488-5f5d-827-c314.ngrok-free.app',
+  api: 'http://localhost:3030', //Acessa os dados do backend
+  //api: 'https://92f6-2804-d45-e063-a900-1488-5f5d-827-c314.ngrok-free.app',
 };
